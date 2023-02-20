0d5225604033156db79823351f15a4a1280f7870
Virag Tamas <petymeg@gmail.com>
2023-02-20 13:54:23 +0100

    Added comments for function behavior. Can be omitted from production version.

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -2,10 +2,15 @@ function fizzBuzz(fizz: number, buzz: number): void {
   const minNumber: number = 1;
   const maxNumber: number = 100;
 
+  //Loop through minNumber to maxNumber
   for (let i: number = minNumber; i <= maxNumber; i++) {
+    //Output if multiple of both parameters
     if (!(i % fizz) && !(i % buzz)) console.log('FizzBuzz');
+    //Output if multiple of first parameter
     else if (!(i % fizz)) console.log('Fizz');
+    //Output if multiple of second parameter
     else if (!(i % buzz)) console.log('Buzz');
+    //Default output
     else console.log(i);
   }
 }
