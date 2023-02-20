adf9cd35a7925bf5c61ebf635fa1812685fb3fd7
Virag Tamas <petymeg@gmail.com>
2023-02-20 13:50:54 +0100

    Added parameter to function to add flexibility

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,10 +1,10 @@
-function fizzBuzz(): void {
+function fizzBuzz(fizz: number, buzz: number): void {
   for (let i: number = 1; i <= 100; i++) {
-    if (!(i % 3) && !(i % 5)) console.log('FizzBuzz');
-    else if (!(i % 3)) console.log('Fizz');
-    else if (!(i % 5)) console.log('Buzz');
+    if (!(i % fizz) && !(i % buzz)) console.log('FizzBuzz');
+    else if (!(i % fizz)) console.log('Fizz');
+    else if (!(i % buzz)) console.log('Buzz');
     else console.log(i);
   }
 }
 
-fizzBuzz();
+fizzBuzz(3, 5);
