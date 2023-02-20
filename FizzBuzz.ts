809468dfa4003f66b0bbde7e5472b1ac167c6cda
Virag Tamas <petymeg@gmail.com>
2023-02-20 18:54:36 +0100

    Variable added for output string

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,15 +1,18 @@
-function fizzBuzz(fizz: number, buzz: number): void {
+function fizzBuzz(fizzNumber: number, buzzNumber: number): void {
   const minNumber: number = 1;
   const maxNumber: number = 100;
+  const fizzString: string = 'Chocolate';
+  const buzzString: string = 'Bar';
 
   //Loop through minNumber to maxNumber
   for (let i: number = minNumber; i <= maxNumber; i++) {
     //Output if multiple of both parameters
-    if (!(i % fizz) && !(i % buzz)) console.log('FizzBuzz');
+    if (!(i % fizzNumber) && !(i % buzzNumber))
+      console.log(fizzString + buzzString);
     //Output if multiple of first parameter
-    else if (!(i % fizz)) console.log('Fizz');
+    else if (!(i % fizzNumber)) console.log(fizzString);
     //Output if multiple of second parameter
-    else if (!(i % buzz)) console.log('Buzz');
+    else if (!(i % buzzNumber)) console.log(buzzString);
     //Default output
     else console.log(i);
   }
